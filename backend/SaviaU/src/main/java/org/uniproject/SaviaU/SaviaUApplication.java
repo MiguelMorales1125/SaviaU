97ec15f0ab6518167281fa007696f9d171bcca46
VICTUS <miguelrin.morales@gmail.com>
2025-09-20 10:36:00 -0500

    Primer commit

---
+++ backend/SaviaU/src/main/java/org/uniproject/SaviaU/SaviaUApplication.java
@@ -0,0 +1,13 @@
+package org.uniproject.SaviaU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaviaUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaviaUApplication.class, args);
+	}
+
+}
---
+++ backend/SaviaU/src/test/java/org/uniproject/SaviaU/SaviaUApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uniproject.SaviaU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaviaUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
